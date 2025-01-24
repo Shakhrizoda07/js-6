64c581cabfaa012e1e554eda50aa7c39e32051f6
Shakhrizoda07 <shakhrizoda752@gmail.com>
2025-01-24 09:19:15 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+// let people = ['Husniddin', 'Ansor', 'Sasha'];
+// console.log(people[1])
+// console.log(people[2] = 'Shoxruz') //изменить значение
+// console.log(people[3] = 'Azim') //добавить значение
+// console.log(people.at(-1))   //взять самое последние значение
+
+// let people = ['Husniddin', 'Ansor', 'Sasha'];
+// console.log(people.pop())
+// console.log(people)  //удаляет значение с самого конца
+
+// let people = ['Husniddin', 'Ansor', 'Sasha'];
+// console.log(people.push('Bonu'))
+// console.log(people)  
+
+// let people = ['Husniddin', 'Ansor', 'Sasha'];
+// console.log(people.shift())
+// console.log(people) 
+
+// let people = ['Husniddin', 'Ansor', 'Sasha'];
+// console.log(people.unshift('Ziyoda'))
+// console.log(people) 
+
+// let user = ['Bonu'];
+// console.log(user.push('Shirina','Ayaulym','Ziyoda'))
+// console.log(user.unshift('Ziyoda','Ayaulym','Shirina'))
+// console.log(user)
+
+let user = alert('Boshlanishda Arrayning uzunligi: 4')
+alert(user)
+
+let userFruits = ['Olma','Gilos','Banan','Shaftoli'];
+console.log(userFruits)
+
+let userTwo = alert('Sizning arrayingizdan malimotlarni olib tashlamoqchimiz')
+alert(userTwo)
+
+let userThree = alert('Arrayingiz uzunligi: 3')
+console.log(userThree)
+
+
+
+
+
+
